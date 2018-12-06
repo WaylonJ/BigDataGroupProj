768e11ce472f5efd1d8ffdd03abf38ded9277fa8
rorihuela <reuben.orihuela@gmail.com>
2018-12-06 00:04:20 -0800

    Added comments to Mapreduce program

--- BookNew.java
+++ BookNew.java
@@ -1,5 +1,10 @@
+/* Reuben Orihuela
+ * Waylon Jones
+ * Casey Au
+ * CS 4650 Final Project
+*/
 
-
+//Import Statements
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -20,22 +25,24 @@ import java.util.Iterator;
 public class BookNew{
 	public static class BookNewMapper extends MapReduceBase implements
 		org.apache.hadoop.mapred.Mapper<LongWritable, Text, Text, IntWritable> {
+	
+	//Constant Value of one
 	private final static IntWritable one = new IntWritable(1);
 
-	/* (non-Javadoc)
-	 * @see org.apache.hadoop.mapred.Mapper#map(java.lang.Object, java.lang.Object, org.apache.hadoop.mapred.OutputCollector, org.apache.hadoop.mapred.Reporter)
-	 */
-	@Override
 	public void map(LongWritable _key, Text value,
 			OutputCollector<Text, IntWritable> output, Reporter reporter)
 			throws IOException {
+		
+		//Convert each row to string and establish delimiter
 		String TempString = value.toString();
 		String[] SingleBookData = TempString.split(",");
 		
-		try {		
+		try {
+			//Add 11th element in CSV to map
 			String together = SingleBookData[10];
 			output.collect(new Text(together), one);
 			
+			//Add 15th element in CSV to map
 			IntWritable myDelay = new IntWritable(Integer.parseInt(SingleBookData[14].toString()));
 			String total = together + "_ArrDelayMins";
 			output.collect(new Text(total), myDelay);
@@ -48,55 +55,40 @@ public class BookNew{
 
 	}
 }
-public static class BookNewReducer extends MapReduceBase implements org.apache.hadoop.mapred.Reducer<Text, IntWritable, Text, IntWritable> {
+	public static class BookNewReducer extends MapReduceBase implements org.apache.hadoop.mapred.Reducer<Text, IntWritable, Text, IntWritable> {
 
-	/* (non-Javadoc)
-	 * @see org.apache.hadoop.mapred.Reducer#reduce(java.lang.Object, java.util.Iterator, org.apache.hadoop.mapred.OutputCollector, org.apache.hadoop.mapred.Reporter)
-	 */
-	@Override
-	public void reduce(Text _key,
-			Iterator<IntWritable> values,
-			OutputCollector<Text,IntWritable> output, 
-			Reporter reporter)
+		public void reduce(Text _key, Iterator<IntWritable> values, OutputCollector<Text,IntWritable> output, Reporter reporter)
 			throws IOException {
-		Text key = _key;
-		int frequencyForYear = 0;
-		while (values.hasNext()) {
-			// replace ValueType with the real type of your value
-			IntWritable value = (IntWritable) values.next();
-			frequencyForYear += value.get();
-			// process value
+			Text key = _key;
+			int frequencyForYear = 0;
+			while (values.hasNext()) {
+				// replace ValueType with actual value type
+				IntWritable value = (IntWritable) values.next();
+				frequencyForYear += value.get();
+			}
+			output.collect(key, new IntWritable(frequencyForYear));
 		}
-		output.collect(key, new IntWritable(frequencyForYear));
 	}
-}
 	public static void main(String[] args) {
 		JobClient client = new JobClient();
-		// Configurations for Job set in this variable
 		JobConf conf = new JobConf(BookNew.class);
 		
-		// Name of the Job
 		conf.setJobName("BookNew");
 		
-		// Data type of Output Key and Value
 		conf.setOutputKeyClass(Text.class);
 		conf.setOutputValueClass(IntWritable.class);
 		
-		// Setting the Mapper and Reducer Class
 		conf.setMapperClass(BookNewMapper.class);
 		conf.setReducerClass(BookNewReducer.class);
 
-		// Formats of the Data Type of Input and output
 		conf.setInputFormat(TextInputFormat.class);
 		conf.setOutputFormat(TextOutputFormat.class);
 		
-		// Specify input and output DIRECTORIES (not files)
 		FileInputFormat.setInputPaths(conf, new Path(args[0]));
 		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
 		
 		client.setConf(conf);
 		try {
-			// Running the job with Configurations set in the conf.
 			JobClient.runJob(conf);
 		} catch (Exception e) {
 			e.printStackTrace();
